ca268a4b377d60ccb9b40a73682eda26d193a7a0
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-11-12 10:03:11 +0600

    php see output

---
+++ main.php
@@ -0,0 +1,31 @@
+<?php
+echo "Hello World";
+
+$a="5";
+$b=10;
+$c=$a+$b;
+echo $c ;
+
+$a=10;
+echo  $a;
+echo "<br>";
+
+#hello me
+// hello me
+echo "Md Ismail hossain";
+print "<br>";
+print "Id: 1285247";
+print "<br>";
+print " Round: 62";
+print "<br>";
+print "Group: WADP";
+// sprintf "NCLC/01";
+print_r("hello");
+printf("me");
+sprintf("hi");
+$d="14";
+$h=98;
+echo $d+$h;
+var_dump($d);
+
+?>
