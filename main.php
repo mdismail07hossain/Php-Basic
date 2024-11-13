cc36096f29cbbfeeda0b53e73b4815a744c6c718
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-11-13 10:36:41 +0600

    ifelse try

--- main.php
+++ main.php
@@ -28,4 +28,25 @@ $h=98;
 echo $d+$h;
 var_dump($d);
 print "<br>"."Ismail"." "."Hosaain";
+echo "<br>";
+// slice text 
+$text = "Hello I am Ismail";
+echo substr($text,11,6);
+echo "<br>";
+// get gerade point 
+$n=40;
+if($n>=90 && $n<=100){
+    echo"Your mark $n You got A+.";
+}elseif($n>=80 ){
+    echo"Your mark $n You got A.";
+}
+elseif($n>=70 ){
+    echo"Your mark $n You got  B+.";
+}
+elseif($n>=60 ){
+    echo"Your mark $n You got C.";
+}else{
+    echo"Your are fail.";
+}
+
 ?>
