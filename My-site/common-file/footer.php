4223c6b0cd098712d1a824126aa57731ad582f34
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-11-16 10:45:15 +0600

    pahp

---
+++ My-site/common-file/footer.php
@@ -0,0 +1,322 @@
+ <footer>
+        <h1>Home php  footer</h1>
+         <section class="main_footer bg-black p-5">
+      <!----------- footer_top  part html start -------------->
+      <div class="footer_top mb-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-6 col-md-2">
+              <div class="footer_top_content">
+                <h6
+                  style="
+                    color: white;
+                    font-size: 16px;
+                    text-transform: uppercase;
+                  "
+                >
+                  About
+                </h6>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >About Us</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Careers</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Awards & Recognition</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Investors</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Bata Franchise</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Share Department</a
+                  >
+                </li>
+              </div>
+            </div>
+            <div class="col-6 col-md-2">
+              <div class="footer_top_content">
+                <h6
+                  style="
+                    color: white;
+                    font-size: 16px;
+                    text-transform: uppercase;
+                  "
+                >
+                  Information
+                </h6>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >TERMS & CONDITIONS</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Payment Options</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Privacy Policy</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Franchise Programme</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Coverage areas</a
+                  >
+                </li>
+              </div>
+            </div>
+            <div class="col-6 col-md-3">
+              <div class="footer_top_content">
+                <h6
+                  style="
+                    color: white;
+                    font-size: 16px;
+                    text-transform: uppercase;
+                  "
+                >
+                  Popular Brands
+                </h6>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Bata</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Hush Puppies</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Power
+                  </a>
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Comfit</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Weinbrenner</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >North Star</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Marie Claire</a
+                  >
+                </li>
+              </div>
+            </div>
+            <div class="col-6 col-md-2">
+              <div class="footer_top_content">
+                <h6
+                  style="
+                    color: white;
+                    font-size: 16px;
+                    text-transform: uppercase;
+                  "
+                >
+                  Customer Service
+                </h6>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >FAQs</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >MyBata</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Bata Stores
+                  </a>
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >All About Feet</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Contact Info</a
+                  >
+                </li>
+                <li style="list-style: none; padding: 3px 0">
+                  <a
+                    class="animation"
+                    href="#"
+                    style="text-decoration: none; font-size: 14px; color: white"
+                    >Report Issue</a
+                  >
+                </li>
+              </div>
+            </div>
+            <div class="col-12 col-md-3">
+              <div class="footer_top_content">
+                <h6
+                  style="
+                    color: white;
+                    font-size: 16px;
+                    text-transform: uppercase;
+                    padding: 0 0 0 19px;
+                  "
+                >
+                  Info
+                </h6>
+                <p style="color: white; font-size: 14; padding: 0 0 0 15px">
+                  Bata Shoe Company Bangladesh Ltd. Tongi Gazipur, Bangladesh.
+                </p>
+                <p style="color: white; font-size: 14px; padding: 0 0 0 15px">
+                  Call us at: 09666200300 (9am-9pm) Email:
+                  bd.customercare@bata.com
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!------------------------------------- footer_top part html end ---------------------------->
+      <!------------------------------------- footer_bottom part html start ---------------------------->
+      <div class="footer_bottom">
+        <div class="container">
+          <hr class="custom-hr" />
+          <div class="row">
+            <div class="col-6">
+              <p style="color: white; font-size: 16px; font-weight: 700">
+                Sign up for our Newsletter
+              </p>
+            </div>
+            <div class="col-6">
+              <div class="footer_icons">
+                <a href="#"
+                  ><i class="fa-brands fa-facebook" style="color: #f2f2f2"></i
+                ></a>
+                <a href="#"
+                  ><i class="fa-brands fa-instagram" style="color: #f2f2f2"></i
+                ></a>
+                <a href="#"
+                  ><i class="fa-brands fa-twitter" style="color: #ffffff"></i
+                ></a>
+                <a href="#"
+                  ><i class="fa-brands fa-youtube" style="color: #ffffff"></i
+                ></a>
+              </div>
+            </div>
+            <div class="col-12 text-center mt-3">
+              <p style="color: #5f5e5e; font-size: 14px">
+                © 2024 Bata Shoe Company Bangladesh Ltd. Tongi, Postal code -
+                1710, Gazipur, Bangladesh
+              </p>
+            </div>
+          </div>
+          <div class="col-12 text-center mt-5">
+            <img src="img/Bata_imge/visa-sq_x300.png" width="24px" alt="" />
+            <img src="img/Bata_imge/masterc-sq_x300.png" width="24px" alt="" />
+            <img src="img/Bata_imge/amex-sq_x300.png" width="24px" alt="" />
+          </div>
+        </div>
+      </div>
+      <!------------------------------------- footer_bottom part html end ---------------------------->
+    </section>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ My-site/homepage.php
@@ -0,0 +1,10 @@
+<?php 
+include('common-file/header.php');
+include('common-file/content.php');
+
+
+include('common-file/footer.php');
+?>
+
+
+   
