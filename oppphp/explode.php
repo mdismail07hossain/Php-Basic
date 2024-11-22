2a9fef8a62aff312b5dc2ca516eed6a8b0211bd2
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-11-22 12:21:17 +0600

    just modify the code

--- oppphp/explode.php
+++ oppphp/explode.php
@@ -2,4 +2,12 @@
 $inforamtion = "My name is Md Ismail Hossain";
 $me=explode(" ",$inforamtion);
 print_r($me);
+
+$nam="Ismail Munna Rafi Ali Rajib Mirza";
+$result=explode(" ",$nam);
+print_r($result);
+// print_r("<br");
+print_r("<br> $result[2] <br>");
+print_r($result[1]);
+
 ?>
--- oppphp/file_contant.php
+++ oppphp/file_contant.php
@@ -1,3 +1,4 @@
 <?php
-echo file_put_contents("stroe.txt","Hello How are you Everyone");
+echo file_put_contents("stroe.txt","Hello How are you Everyone.\n");
+echo file_put_contents("stroe.txt","Everyone Hello ismail we are fine and you?",FILE_APPEND);
 ?>
