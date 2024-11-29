d2f9bcb24ef9b1e1959fbd78ff443e1c8838346f
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-11-29 20:14:55 +0600

    just modify the data save file

--- evdinces/evidence/authentikcation/file_upload/upload_file.php
+++ evdinces/evidence/authentikcation/file_upload/upload_file.php
@@ -1,48 +1,61 @@
 <?php
-if(isset($_POST["btnsubmit"])){
-    $file=$_FILES["myfile"] ["name"];
-    $size=$_FILES["myfile"]["size"] ;
-    
-    // var_dump($file);
-    $filetamp=$_FILES["myfile"]["tmp_name"];
-    $kb=100*1024;
-    if($size>$kb){
-        $folder="uploads/";
-        echo "Upload a mixmuma 100KB file";
-    }else{
-        $folder="uploads/";
-        echo "file Upload seccus fully";
-        move_uploaded_file("$filetamp","$folder.$file",);
-        
+
+if (isset($_POST["submit"])) {
+    $fileName = $_FILES["fileToUpload"]["name"];
+    $temp = $_FILES["fileToUpload"]["tmp_name"];
+    $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    $imgLocaton = "uploads/";
+    $file_size = $_FILES["fileToUpload"]["size"];
+    $kb = $file_size / 1024;
+
+    if (($kb > 100) || !in_array($fileType, ["jpg", "png", "jpeg", "gif"])) {
+        if ($kb > 100) {
+            $msg1 = "Image is too large. Your image must be a maximum of 100 KB.";
+        }
+        if (!in_array($fileType, ["jpg", "png", "jpeg", "gif"])) {
+            $msg2 = "Also Sorry, only jpg, png, jpeg, or gif formats are allowed!";
+        }
+    } else {
+        move_uploaded_file($temp, $imgLocaton . $fileName);
+        echo "Successfully Uploaded!";
     }
+
+
 }
+
+
 ?>
+
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>filr_upload</title>
-</head>
+<html>
+
 <body>
-    <form action="#" method="post" enctype="multipart/form-data">
-    Input_a_File: <br>    
-    <input type="file" name="myfile"><br><br>
-        <input type="submit" name="btnsubmit">
+    <?php
+    echo isset($msg1) ? $msg1 : '';
+    echo "<br>";
+    echo isset($msg2) ? $msg2 : '';
+    ?>
+    <form action="" method="post" enctype="multipart/form-data" style="margin-top:15px;">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload"><br>
+        <input type="submit" value="Upload Image" name="submit"><br>
     </form>
 
-  
 </body>
+
 </html>
 
-<?php 
- $folder="uploads/";
- $images=glob($folder);
-if(count($imgaes)>0){
-    foreach($imgaes as $img){
-        echo "<img src='.$img .'alt='Upload image'>";
+<?php
+// Display all uploaded images from the "uploads" directory
+$imgLocation = "uploads/";
+$images = glob($imgLocation . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+
+if (count($images) > 0) {
+    foreach ($images as $image) {
+        echo '<img src="' . $image . '" alt="Uploaded Image">';
     }
-}else{
-        echo "no image here!";
-    }
+} else {
+    echo "No images uploaded yet.";
+}
 ?>
