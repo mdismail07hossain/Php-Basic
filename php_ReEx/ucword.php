152bdb103dc7f1b0e8fcf394b2fd0efa9f597544
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-11-30 11:10:16 +0600

    file upload form design modify

--- php_ReEx/ucword.php
+++ php_ReEx/ucword.php
@@ -3,4 +3,10 @@ $a="This is our class";
 echo ucfirst($a);
 echo "<br>";
 echo ucwords($a);
+
+echo "<br>";
+$letter ="hello I am md ismail hossain ";
+echo strtolower($letter);
+echo "<br>";
+echo strtoupper($letter);
 ?>
