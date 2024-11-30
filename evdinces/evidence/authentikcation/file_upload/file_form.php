06d7e4835a6dc5be541b6ef096f6350a9c039d25
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-11-30 09:35:38 +0600

    file upload form design modify

--- evdinces/evidence/authentikcation/file_upload/file_form.php
+++ evdinces/evidence/authentikcation/file_upload/file_form.php
@@ -6,6 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File Upload</title>
 </head>
+<style>
+    form{
+        margin: 0 auto;
+        width: 400px;
+        padding: 30px 40px ;
+        background: linear-gradient(to left,red,orange);
+        color: white;
+        font-size: 22px;
+        font-weight: 600;
+        text-transform: capitalize;
+        border-radius: 10px;
+        box-shadow: rgba(0, 0, 0, 0.35)0px 5px 15px;
+    }
+    input{
+        width: 100%;
+        outline: none;
+        border: 2px solid green;
+    }
+    h2{
+        text-align: center;
+    }
+</style>
 <body>
 
     <h2>Upload File</h2>
