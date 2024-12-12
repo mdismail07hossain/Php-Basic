8aa31be4635b2f2f9f9ca73589179d3d531ce769
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-12-12 10:11:27 +0600

    modify

--- relations_tables/product_insormation.php
+++ relations_tables/product_insormation.php
@@ -1,4 +1,30 @@
 <?php 
 $connt=mysqli_connect("localhost","root","","trianeere_info");
 
+if(isset($_POST["subbtn"])){
+    $name=$_POST['name'];
+    // $email=$_POST['email'];
+    $number=$_POST['number'];
+    // insert query create here 
+    $connt->query("call brand_name('$name','$number')");
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+</head>
+<body>
+    <h2>Product Form</h2>
+    <form action="#" method="POST">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="number">Phone Number:</label><br>
+        <input type="tel" id="number" name="number" required><br><br>
+        <input type="submit" value="Submit" name="subbtn">
+    </form>
+</body>
+</html>
