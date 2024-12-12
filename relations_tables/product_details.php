5f576b98b4ad209b20cab8624cf572ed95c65bd2
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-12-12 12:32:09 +0600

    modify

--- relations_tables/product_details.php
+++ relations_tables/product_details.php
@@ -1,31 +1,35 @@
-<?php 
-$bds=mysqli_connect("localhost","root","","trianeere_info");
 
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Form</title>
+    <title>Product Form</title>
 </head>
 <body>
-    <h2>Contact Form</h2>
-    <form action="#" method="POST">
+    <h2>Product Form</h2>
+    <form action="" method="POST">
         <label for="name">Name:</label><br>
         <input type="text" id="name" name="name" required><br><br>
-
-        <label for="email">product_brand:</label><br>
-       <select name="" id="">
-        <option value=""></option>
-       </select>
-
         <label for="number">Product_Price:</label><br>
-        <input type="tel" id="number" name="number" required><br><br>
+        <input type="text" id="number" name="number" required><br><br>
 
-        <input type="submit" value="Submit" name="subbtn">
+
+        <label for="email">product_brand:</label>
+       <select name="manufc" >
+            <?php
+            $bds=mysqli_connect("localhost","root","","trianeere_info");
+            $manu=$bds->query("select * from brand_information");
+            while(list($id,$name)=$manu->fetch_row()){
+             echo "<option value='$id'>$name</option>";
+            }
+        
+        ?>
+       </select><br><br>
+               <input type="submit" value="Submit" name="subbtn">
+
+        
     </form>
 </body>
 </html>
