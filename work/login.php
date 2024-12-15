4ad0ebcaf12a30adb9c02365a90a32db9a8e1d05
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-12-15 09:54:23 +0600

    modify

--- work/login.php
+++ work/login.php
@@ -4,12 +4,11 @@ session_start();
 if(isset($_POST["btnsubmit"])){
     $name=$_POST["name"];
     $password=$_POST["passaword"];
+    $email=$_POST["email"];
+    $emailchaker= '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
     // $message=$_POST["message"];
 
-    if($name==="Ismail"&&$password==="123"){
-        $_SESSION["ismail"]=$name;
-        header("location:form.php");
-    }else{
+  
         if($name!=="Ismail"){
             $message="<div class='msg'>Your user name is incorrect.</div>";
         }
@@ -17,7 +16,17 @@ if(isset($_POST["btnsubmit"])){
             $message1="<div class='msg'>Your password  is incorrect.</div>";
         }
         
+    
+  if (!preg_match($emailchaker, $email)) {
+       // Valid email
+       $message2="<div class='msg'>Please Enter a valid email..</div>"; // Invalid email
+    } else {
+    
+  if($name==="Ismail"&&$password==="123"){
+        $_SESSION["ismail"]=$name;
+        header("location:form.php");
     }
+}
 }
 ?>
 <!DOCTYPE html>
@@ -28,9 +37,13 @@ if(isset($_POST["btnsubmit"])){
     <title>login</title>
 </head>
 <style>
+    body{
+        background-color: gray;
+    }
     form{
-        background-color: skyblue;
-        color: white;
+        background-color: white;
+        box-shadow: rgba(0, 0, 0, 0.35)0px 5px 15px;
+        color: gray;
         border-radius: 10px;
         padding: 30px 40px;
         
@@ -54,9 +67,9 @@ if(isset($_POST["btnsubmit"])){
     }
 
     .msg {
-        text-align: center;
-        font-size: 22px;
-        font-weight: 700;
+        text-align: left;
+        font-size: 18px;
+        font-weight: 600;
         color: red;
     }
 </style>
@@ -65,17 +78,28 @@ if(isset($_POST["btnsubmit"])){
      <form action="#" method="post">
         <label for="name">User_name</label>
         <input type="text" name="name">
+        <?php
+        echo isset($message) ? $message: "";
+        ?>
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email">
+        <?php
+        echo isset($message2) ? $message2: "";
+        ?>
         <label for="password">Password</label>
         <input type="password" name="passaword">
+        <?php
+        echo isset($message1) ? $message1: "";
+        ?>
         <button  type="submit" name="btnsubmit">Submit</button>
     </form>
     <!-- <div name="message"></div> -->
 
     <?php 
     
-    echo isset($message) ? $message :"";
+    // echo isset($message) ? $message :"";
     echo "<br>";
-    echo isset($message1) ? $message1 :"";
+    // echo isset($message1) ? $message1 :"";
     ?>
    
     
