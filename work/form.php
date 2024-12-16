28b7c752ff9791d935b4204e372e2ed08b147364
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-12-16 22:40:48 +0600

    modify

--- work/form.php
+++ work/form.php
@@ -120,11 +120,40 @@ if (isset($_POST["submit"])) {
          cursor: pointer;
        
     }
+     #num {
+        padding: 7px 10px ;
+        border-radius: 5px;
+        outline:none;
+        background-color:green;
+        color:white;
+        border:none;
+        box-shadow:rgba(0,0,0,0.35)0px 5px 15px;
+
+    }
+    input{
+        padding: 7px 10px ;
+        display:block;
+        margin-bottom:10px ;
+        border-radius: 5px;
+    }
+    section{
+        margin: 0 auto;
+        width: 300px;
+    }
+    .form{
+        background-color:grey;
+        padding: 30px 40px;
+        color:white;
+        border:none;
+        border-radius: 5px;
+        box-shadow:rgba(0,0,0,0.35)0px 5px 15px;
+        
+    }
     /* design part css end  */
 </style>
 <body>
 
-    <form action="" method="post" enctype="multipart/form-data" style="margin-top:15px;">
+    <form class="form" action="" method="post" enctype="multipart/form-data" style="margin-top:15px;">
         Select image to upload:
         <br><br>
         <input type="file" name="fileToUpload" id="fileToUpload">
@@ -138,6 +167,27 @@ if (isset($_POST["submit"])) {
       
         <button name="button" ><a href="logout.php">LogOut</a></button>
     </form>
+    <section>
+    <form action="#" method="post">  
+    Enter the Number:<br>  
+    <input type="number" name="number" id="number">  
+    <input id="num" type="submit" name="submit" value="Submit" />  
+<?php   
+    if($_POST){  
+        $fact = 1;  
+        //getting value from input text box 'number'  
+        $number = $_POST['number'];  
+        echo "<h3 style='text-align: center;'>Factorial of $number:</h3>
+        ";  
+        //start loop  
+        for ($i = 1; $i <= $number; $i++){         
+            $fact = $fact * $i;  
+            }  
+            echo "<h3 style='text-align: center;'> $fact . </h3>";  
+    }  
+?> 
+</form> 
+</section> 
     <section class="php_img">
 <?php
 // Display all uploaded images from the "uploads" directory
@@ -157,5 +207,16 @@ if (count($images) > 0) {
 </body>
 
 </html>
+<html>  
+<head>  
+<title>Factorial Program using loop in PHP</title>  
+</head>
+<style>
+     
+</style>  
+<body>  
 
+    
+</body>  
+</html>  
 <!-- html part end  -->
