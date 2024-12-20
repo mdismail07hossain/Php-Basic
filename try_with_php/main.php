b86d52e9bbe6f0f74e10fb3ffb937a542a2f1aee
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-12-20 19:16:07 +0600

    try php

---
+++ try_with_php/main.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prosudouer</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <label for="brand">Brand_Name:</label>
+        <select name="brand" id="">
+            <option value="souny">Souny</option>
+            <option value="BmW">BmW</option>
+            <option value="RRR">RRR</option>
+            <option value="apple">apple</option>
+            <option value="Hp">Hp</option>
+            <option value="Ascr">Ascr</option>
+            <option value="walton">walton</option>
+            <option value="toyota">toyota</option>
+            <option value="redmi">redmi</option>
+            <option value="oppo">oppo</option>
+        </select>
+    </form>
+</body>
+</html>
