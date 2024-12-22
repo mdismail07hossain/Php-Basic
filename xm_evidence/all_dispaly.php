165d16f1d1111b142181a90b8a2c2ce90f1ad225
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-12-22 10:14:13 +0600

    hello

---
+++ xm_evidence/all_dispaly.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <h1>Product Display</h1>
+        <table>
+            <tr>
+                <th>Brand</th>
+                <th>Address</th>
+                <th>Contact</th>
+                <th>Product Name</th>
+                <th>Product Price</th>
+            </tr>
+            <?php
+                $connt = mysqli_connect("localhost", "root", "", "student_info");
+                $dis = $connt->query("SELECT * FROM product_v");
+                while (list($name, $address, $contact, $pname, $price) = $dis->fetch_row()) {
+                   
+                         echo "
+                    <tr>
+                        <td>$name</td>
+                        <td>$address</td>
+                        <td>$contact</td>
+                        <td>$pname</td>
+                        <td>$price</td>
+                    </tr>
+                    ";
+                    }
+                   
+              
+            ?>
+        </table>
+    </div>
+</body>
+</html>
