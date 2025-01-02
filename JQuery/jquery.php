0efbb7902a21946f06a355683da312a6cd54ee96
mdismail07hossain <mdismail07.hossain@gmail.com>
2025-01-02 12:07:21 +0600

    hello

---
+++ JQuery/jquery.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jquery Test try</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- jQuery CDN (from Google) -->
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
+
+</head>
+<body>
+    <div class="one"></div>
+    <div class="one"></div>
+    <script src="./js/jry.js"></script>
+    <script>
+        $('.one').hide(5000);
+        $('.one').hide(5000);
+    </script>
+</body>
+</html>
