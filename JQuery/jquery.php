2c01e38d7b1c148a1ff0aae7f00edc4b42498c25
mdismail07hossain <mdismail07.hossain@gmail.com>
2025-01-02 12:16:35 +0600

    hello

--- JQuery/jquery.php
+++ JQuery/jquery.php
@@ -3,20 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jquery Test try</title>
+    <title>jQuery Test</title>
     <link rel="stylesheet" href="style.css">
     <!-- jQuery CDN (from Google) -->
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
-    <div class="one"></div>
-    <div class="one"></div>
-    <button onclick="animi">Start Animation</button>
-    <script src="./js/jry.js"></script>
+    <div class="one" style="width: 100px; height: 100px; background-color: red; margin: 10px;"></div>
+    <div class="one" style="width: 100px; height: 100px; background-color: blue; margin: 10px;"></div>
+    <button onclick="animi()">Start Animation</button>
+
     <script>
-        function animi{
-            $('.one').hide(5000);
+        function animi(){
+            // Hide all elements with the class 'one' with a 5 second duration
             $('.one').hide(5000);
         }
     </script>
