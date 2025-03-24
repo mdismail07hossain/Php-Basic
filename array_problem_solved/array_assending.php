453fb388eb1029394df3fbeb76e5eec3bc9bc364
mdismail07hossain <mdismail07.hossain@gmail.com>
2025-03-25 00:07:01 +0600

    array problem solved

---
+++ array_problem_solved/array_assending.php
@@ -0,0 +1,7 @@
+<!---------------------- PHP-তে Ascending Order এর উদাহরণ: -------------------->
+<!-- ১. Array Sorting (সাধারণ array): --->
+<?php
+$numbere = array(5, 7, 3, 9, 2, 1);
+sort($number);
+print_r($number);
+?>
